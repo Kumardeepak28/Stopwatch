bec1f5933d0136e9cf15322bc23b597a5136d889
Deepak <kumardeepakbr28@gmail.com>
2024-01-21 20:43:28 +0530

    stopwatch

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ document.getElementById("start").addEventListener("click", startTimer);
 document.getElementById("stop").addEventListener("click", stopTimer);
 document.getElementById("reset").addEventListener("click", resetTimer);
 
+const btn = document.getElementById("start");
+
+
 function resetTimer() {
     clearInterval(timer)
     timerInMs = 0;
     updateTime(timerInMs);
+    btn.disabled = false;
 }
 function stopTimer(){
     clearInterval(timer);
+    btn.disabled = false;
 }
 function startTimer() {
     timer = setInterval(()=>{
     timerInMs+=10;
     updateTime(timerInMs);
     },10)
+    btn.disabled = true;
 }
+
+
 function updateTime(timerInMS) {
 
     // timerInMS = 2400 (2 sec and 400 ms)
@@ -31,8 +39,5 @@ document.getElementById("min").innerText = min<10 ? "0" + min%60 : min%60;
 
 document.getElementById("sec").innerText = sec<10 ? "0" + sec%60 : sec%60;
 
-document.getElementById("ms").innerText = ms;
+document.getElementById("ms").innerText = ms < 10 ? "0" + ms : ms;
 }
-
-
-
